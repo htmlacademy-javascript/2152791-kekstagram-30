7f6a18afe66b4084752d5cf74161bb6d296f0d1a
T-800 <euronymous.ts@yandex.ru>
2023-10-19 19:44:06 +0300

    More details

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+let numbersId = [];
+let numbersPhoto = [];
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+
 const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадр В конце концов это просто непрофессионально.',
@@ -23,35 +28,25 @@ const DESCRIPTION = [
 ];
 
 //Generate numbers
-const generateRandomNumber = function (min, max) {
+const generateRandomNumber = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
 
+//Generate ID
+for (let i = 1; i <= 25; i++) {
+  numbersId.push(i);
+  numbersId = numbersId.sort(() => Math.random() - 0.5);
+  numbersPhoto.push(i);
+  numbersPhoto = numbersPhoto.sort(() => Math.random() - 0.5);
+}
+
 //Create main function
 const createRandomSpecification = () => {
-  const getRandomLikes = generateRandomNumber(15, 200);
+  const getRandomLikes = generateRandomNumber(MIN_LIKES, MAX_LIKES);
   const getRandomDescription = generateRandomNumber(0, DESCRIPTION.length - 1);
-
-  //Generate list of numbers
-  const generateListNumbers = function (min, max) {
-    const previousNumber = [];
-    return function () {
-      let currentNumber = generateRandomNumber(min, max);
-      if (previousNumber.length >= (max - min + 1)) {
-        return null;
-      }
-      while (previousNumber.includes(currentNumber)) {
-        currentNumber = generateRandomNumber(min, max);
-      }
-      previousNumber.push(currentNumber);
-      return currentNumber;
-    };
-  };
-  const generateNumber = generateListNumbers(1, 25);
-
 
   //Create comment
   const createUser = () => {
@@ -69,15 +64,12 @@ const createRandomSpecification = () => {
   };
   const someComments = Array.from({ length: generateRandomNumber(0, 30) }, createUser);
 
-
   return {
-    id: generateNumber(),
-    url: 'photos/' + generateNumber() + '.jpg',
+    id: numbersId.pop(),
+    url: 'photos/' + numbersPhoto.pop() + '.jpg',
     description: DESCRIPTION[getRandomDescription],
     likes: getRandomLikes,
     comments: someComments,
   };
 };
 const randomSpecification = Array.from({ length: 25 }, createRandomSpecification);
-
-console.log(randomSpecification);
